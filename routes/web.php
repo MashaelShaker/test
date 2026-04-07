6634d466debab61079a497284b801a9d4806a735
AsailAlnifiai <asailalnifiai@hotmail.com>
2026-04-08 01:16:10 +0300

    resolve route conflicts and add api aliases

--- routes/web.php
+++ routes/web.php
@@ -3,38 +3,39 @@
 use App\Http\Controllers\BoxController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\OAuthController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\BoxController as ApiBoxController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+    if (auth()->check()) {
+        return redirect('/boxes');
+    }
+    return redirect()->route('oauth.redirect');
+});
 
-// Salla Auth OAuth routes
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('welcome');
+Route::get('/oauth/redirect', [OAuthController::class, 'redirect'])->name('oauth.redirect');
+Route::get('/oauth/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
+
+Route::middleware('auth')->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
-    Route::get('/oauth/redirect', [OAuthController::class, 'redirect'])->name('oauth.redirect');
-    Route::get('/oauth/callback', [OAuthController::class, 'callback'])->name('oauth.callback');
 
+    // Product API routes
+    Route::get('/api/products', [ProductController::class, 'index']);
+    Route::get('/api/products/{id}', [ProductController::class, 'show']);
 
+    // Web view routes for boxes
     Route::get('/boxes', [BoxController::class, 'index']);
     Route::get('/boxes/{id}', [BoxController::class, 'show']);
     Route::delete('/boxes/{id}', [BoxController::class, 'destroy']);
     Route::get('/boxes/{id}/edit', [BoxController::class, 'edit']);
-    });
+
+    // API routes for boxes
+    Route::get('/api/boxes', [BoxController::class, 'index']);
+    Route::get('/api/boxes/{id}', [BoxController::class, 'show']);
+    Route::post('/api/boxes', [BoxController::class, 'store']);
+});
+
